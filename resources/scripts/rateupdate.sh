ab3a020d2b2a281ce832e842b79a5b1a34c74832
Shahe Tajiryan <shtajiryan@hotmail.com>
2023-05-10 03:24:04 +0400

    added as of

--- resources/scripts/rateupdate.sh
+++ resources/scripts/rateupdate.sh
@@ -15,4 +15,5 @@ bestbuybank=$(cat /tmp/rates/index.html | grep -B 6 $bestbuy | grep -o '^<img al
 # echo $bestsellbank
 # echo $bestbuybank
 
-echo "<meta charset="utf-8"><h1> Best sell rate is $bestsell to bank $bestsellbank and best buy rate is $bestbuy from bank $bestbuybank </h1>" > /var/www/rate-website/index.html
+echo "<meta charset="utf-8"><h1> Best sell rate is ֏$bestsell to bank $bestsellbank and best buy rate is ֏$bestbuy from bank $bestbuybank </h1>" > /var/www/rate-website/index.html
+echo "as of $(date)" >> /var/www/rate-website/index.html
