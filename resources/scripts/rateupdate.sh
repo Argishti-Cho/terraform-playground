3e35890e3155e964fd28559c1f9d0deebbe6a115
Shahe Tajiryan <shtajiryan@hotmail.com>
2023-05-10 01:02:08 +0400

    only nginx left to fix

--- resources/scripts/rateupdate.sh
+++ resources/scripts/rateupdate.sh
@@ -1,5 +1,4 @@
-mkdir /tmp/rates
-wget -O /tmp/rates/index.html https://rate.am/ 
+wget -N -P /tmp/rates/ https://rate.am/
 
 cat /tmp/rates/index.html | awk '/^<tr id="[0-9a-zA-Z-]*">$/{p=NR+8} NR==p' | sed 's/[^0-9.]//g' > /tmp/rates/bestsell.txt
 cat /tmp/rates/index.html | awk '/^<tr id="[0-9a-zA-Z-]*">$/{p=NR+9} NR==p' | sed 's/[^0-9.]//g' > /tmp/rates/bestbuy.txt
@@ -7,11 +6,13 @@ cat /tmp/rates/index.html | awk '/^<tr id="[0-9a-zA-Z-]*">$/{p=NR+9} NR==p' | se
 bestsell=$(sort -n /tmp/rates/bestsell.txt | tail -n 1)
 bestbuy=$(sort -n /tmp/rates/bestbuy.txt | head -n 1)
 
-echo $bestsell
-echo $bestbuy
+# echo $bestsell
+# echo $bestbuy
 
 bestsellbank=$(cat /tmp/rates/index.html | grep -B 5 $bestsell | grep -o '^<img align="left" border="0" width="32" height="16" alt=.*>.*</a></td>$' | sed "s/.*'>//" | sed 's/<\/a><\/td>$//')
 bestbuybank=$(cat /tmp/rates/index.html | grep -B 6 $bestbuy | grep -o '^<img align="left" border="0" width="32" height="16" alt=.*>.*</a></td>$' | sed "s/.*'>//" | sed 's/<\/a><\/td>$//')
 
-echo $bestsellbank
-echo $bestbuybank
+# echo $bestsellbank
+# echo $bestbuybank
+
+echo "<h1> Best sell rate is $bestsell to bank $bestsellbank and best buy rate is $bestbuy from bank $bestbuybank </h1>" > /home/ubuntu/resources/rate-website/source/index.html
