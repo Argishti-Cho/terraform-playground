ff7b37b3148aed17cd8395058d2f5c9926727627
Shahe Tajiryan <shtajiryan@hotmail.com>
2023-05-16 02:55:53 +0400

    updated parsing, need to fix every half minute

--- resources/scripts/rateupdate.sh
+++ resources/scripts/rateupdate.sh
@@ -1,16 +1,18 @@
-wget -N -P /tmp/rates/ https://rate.am/
+#!/bin/bash
 
-cat /tmp/rates/index.html | awk '/^<tr id="[0-9a-zA-Z-]*">$/{p=NR+8} NR==p' | sed 's/[^0-9.]//g' > /tmp/rates/bestsell.txt
-cat /tmp/rates/index.html | awk '/^<tr id="[0-9a-zA-Z-]*">$/{p=NR+9} NR==p' | sed 's/[^0-9.]//g' > /tmp/rates/bestbuy.txt
+wget -O - https://rate.am | sed -n '/^<tr id="/,$p' | sed '/^<td colspan="/Q' > /tmp/index.html
 
-bestsell=$(sort -n /tmp/rates/bestsell.txt | tail -n 1)
-bestbuy=$(sort -n /tmp/rates/bestbuy.txt | head -n 1)
+cat /tmp/index.html | awk '/^<tr id="[0-9a-zA-Z-]*">$/{p=NR+8} NR==p' | sed 's/[^0-9.]//g' > /tmp/bestsell.txt
+cat /tmp/index.html | awk '/^<tr id="[0-9a-zA-Z-]*">$/{p=NR+9} NR==p' | sed 's/[^0-9.]//g' > /tmp/bestbuy.txt
+
+bestsell=$(sort -n /tmp/bestsell.txt | tail -n 1)
+bestbuy=$(sort -n /tmp/bestbuy.txt | head -n 1)
 
 # echo $bestsell
 # echo $bestbuy
 
-bestsellbank=$(cat /tmp/rates/index.html | grep -B 5 $bestsell | grep -o '^<img align="left" border="0" width="32" height="16" alt=.*>.*</a></td>$' | sed "s/.*'>//" | sed 's/<\/a><\/td>$//')
-bestbuybank=$(cat /tmp/rates/index.html | grep -B 6 $bestbuy | grep -o '^<img align="left" border="0" width="32" height="16" alt=.*>.*</a></td>$' | sed "s/.*'>//" | sed 's/<\/a><\/td>$//')
+bestsellbank=$(cat /tmp/index.html | grep -B 5 $bestsell | grep -o '^<img align="left" border="0" width="32" height="16" alt=.*>.*</a></td>$' | sed 's/<\/a><\/td>$//' | sed -r 's/.*>([^>]+)$/\1/g')
+bestbuybank=$(cat /tmp/index.html | grep -B 6 $bestbuy | grep -o '^<img align="left" border="0" width="32" height="16" alt=.*>.*</a></td>$' | sed 's/<\/a><\/td>$//' | sed -r 's/.*>([^>]+)$/\1/g')
 
 # echo $bestsellbank
 # echo $bestbuybank
