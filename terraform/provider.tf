fea6b085b57f9fb080f469ebcd7027180987571b
Shahe Tajiryan <shtajiryan@hotmail.com>
2023-05-17 17:24:06 +0400

    restored s3 backend

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,11 +6,11 @@ terraform {
     }
   }
 
-  # backend "s3" {
-  #   bucket = "shahe-terraform-state-bucket"
-  #   key = "state/terraform.tfstate"
-  #   region = "us-east-1"
-  # }
+  backend "s3" {
+    bucket = "shahe-terraform-state-bucket"
+    key = "state/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
