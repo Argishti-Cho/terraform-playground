cb032cd6ec9403beb800125f5fe8cafcb99992af
Shahe Tajiryan <shtajiryan@hotmail.com>
2023-05-30 20:01:21 +0400

    test

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -15,6 +15,6 @@ terraform {
 
 provider "aws" {
   region     = var.aws_region
-  access_key = $tf-var-aws-access
-  secret_key = $tf-var-aws-secret
+  access_key = $(tf-var-aws-access)
+  secret_key = $(tf-var-aws-secret)
 }
